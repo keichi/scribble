cf7823e74bcba1bdc1920bedf4dae4a7c2a25b0a
Keichi Takahashi <keichi.t@me.com>
2015-09-15 12:28:49 +0900

    Add test for add note API

--- handler/note_handler_test.go
+++ handler/note_handler_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
 
+	"github.com/keichi/scribble/auth"
 	"github.com/keichi/scribble/model"
 )
 
@@ -55,7 +56,42 @@ func TestListNotes(t *testing.T) {
 
 	notes := resp.([]interface{})
 	assert.Equal(len(notes), 3)
-	assert.Equal(notes[0].(map[string]interface{})["title"], "Test Title 1")
-	assert.Equal(notes[1].(map[string]interface{})["title"], "Test Title 2")
-	assert.Equal(notes[2].(map[string]interface{})["title"], "Test Title 3")
+	assert.Equal("Test Title 1", notes[0].(map[string]interface{})["title"])
+	assert.Equal("Test Title 2", notes[1].(map[string]interface{})["title"])
+	assert.Equal("Test Title 3", notes[2].(map[string]interface{})["title"])
+}
+
+func TestAddNote(t *testing.T) {
+	assert := assert.New(t)
+
+	dbMap := initDb()
+	defer dbMap.Db.Close()
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "db", dbMap)
+	ctx = context.WithValue(ctx, "auth", &auth.AuthContext{})
+
+	server := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			AddNote(ctx, w, r)
+		},
+	))
+	defer server.Close()
+
+	resp := request(t, server, http.StatusOK, map[string]interface{}{
+		"title":    "Test Title",
+		"content":  "lorem ipsum dolor sit amet consetetur.",
+		"owner_id": 0,
+	})
+	assert.NotNil(resp)
+
+	note := resp.(map[string]interface{})
+	assert.Equal("Test Title", note["title"])
+	assert.Equal("lorem ipsum dolor sit amet consetetur.", note["content"])
+	assert.EqualValues(0, note["ownerId"])
+	assert.NotZero(note["createdAt"])
+	assert.NotZero(note["updatedAt"])
+
+	count, err := dbMap.SelectInt("SELECT COUNT(id) FROM notes")
+	assert.Nil(err)
+	assert.EqualValues(1, count)
 }
