2238ab693e584a05b967b1752046d689a3a52697
Keichi Takahashi <keichi.t@me.com>
2015-09-15 12:00:59 +0900

    Add test for note list API

---
+++ handler/note_handler_test.go
@@ -0,0 +1,61 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/context"
+
+	"github.com/keichi/scribble/model"
+)
+
+func TestListNotes(t *testing.T) {
+	assert := assert.New(t)
+
+	dbMap := initDb()
+	defer dbMap.Db.Close()
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "db", dbMap)
+
+	dbMap.Insert(&model.Note{
+		Id:        0,
+		Title:     "Test Title 1",
+		Content:   "lorem ipsum dolor sit amet consetetur.",
+		OwnerId:   0,
+		CreatedAt: 1442284669000,
+		UpdatedAt: 1442284669000,
+	}, &model.Note{
+		Id:        0,
+		Title:     "Test Title 2",
+		Content:   "lorem ipsum dolor sit amet consetetur.",
+		OwnerId:   0,
+		CreatedAt: 1442284669000,
+		UpdatedAt: 1442284669000,
+	}, &model.Note{
+		Id:        0,
+		Title:     "Test Title 3",
+		Content:   "lorem ipsum dolor sit amet consetetur.",
+		OwnerId:   0,
+		CreatedAt: 1442284669000,
+		UpdatedAt: 1442284669000,
+	})
+
+	server := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			ListNotes(ctx, w, r)
+		},
+	))
+	defer server.Close()
+
+	resp := request(t, server, http.StatusOK, nil)
+	assert.NotNil(resp)
+
+	notes := resp.([]interface{})
+	assert.Equal(len(notes), 3)
+	assert.Equal(notes[0].(map[string]interface{})["title"], "Test Title 1")
+	assert.Equal(notes[1].(map[string]interface{})["title"], "Test Title 2")
+	assert.Equal(notes[2].(map[string]interface{})["title"], "Test Title 3")
+}
