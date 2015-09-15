3155886d233be567165e0cbc6fed8823e750d90a
Keichi Takahashi <keichi.t@me.com>
2015-09-15 15:05:25 +0900

    Add share state field to note table

--- model/model.go
+++ model/model.go
@@ -17,11 +17,19 @@ type Session struct {
 	UpdatedAt int64  `db:"updated_at"`
 }
 
+type ShareState int8
+
+const (
+	SHARE_STATE_PUBLIC int = iota
+	SHARE_STATE_PRIVATE
+)
+
 type Note struct {
-	Id        int64  `db:"id" json"id,omitempty"`
-	Title     string `db:"title" json:"title"`
-	Content   string `db:"content" json:"content"`
-	OwnerId   int64  `db:"owner_id" json:"ownerId"`
-	CreatedAt int64  `db:"created_at" json:"createdAt,omitempty"`
-	UpdatedAt int64  `db:"updated_at" json:"updatedAt,omitempty"`
+	Id         int64      `db:"id" json"id,omitempty"`
+	Title      string     `db:"title" json:"title"`
+	Content    string     `db:"content" json:"content"`
+	OwnerId    int64      `db:"owner_id" json:"ownerId"`
+	ShareState ShareState `db:"share_state" json:"share_state"`
+	CreatedAt  int64      `db:"created_at" json:"createdAt,omitempty"`
+	UpdatedAt  int64      `db:"updated_at" json:"updatedAt,omitempty"`
 }
