9001db964eff6b964df3d4990b24dc03b4ae0d0d
Keichi Takahashi <keichi.t@me.com>
2015-09-16 08:54:14 +0900

    Add initial migrations

---
+++ model/migrations/0001_first.up.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username VARCHAR(64),
+  password_hash VARCHAR(64),
+  email VARCHAR(128),
+  created_at INT64,
+  updated_at INT64
+);
+
+CREATE TABLE sessions (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  token VARCHAR(64),
+  user_id INT64,
+  created_at INT64,
+  updated_at INT64
+);
+
+CREATE TABLE notes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title VARCHAR(256),
+  content TEXT,
+  owner_id INT64,
+  share_state INT8
+  created_at INT64,
+  updated_at INT64
+);
