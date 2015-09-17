9f1e1e87cec1f1a9db572c9031c50b7b0ba39030
Keichi Takahashi <keichi.t@me.com>
2015-09-17 15:06:05 +0900

    Add panic handler

---
+++ handler/panic.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"golang.org/x/net/context"
+	"log"
+	"net/http"
+
+	"github.com/guregu/kami"
+)
+
+func Panic(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+
+	encoder := json.NewEncoder(w)
+	e := kami.Exception(ctx)
+	resp := map[string]string{
+		"message": fmt.Sprintf("Handler panic: %v", e),
+		"context": fmt.Sprint(ctx),
+	}
+
+	encoder.Encode(resp)
+
+	log.Println("Panicked in kami handler")
+	log.Printf("Panic detail: %v", e)
+	log.Printf("Current context: %v", ctx)
+}
