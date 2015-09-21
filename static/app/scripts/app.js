78b79dba5d78f66edb2e07e2f7086d34f4e48b64
Keichi Takahashi <keichi.t@me.com>
2015-09-21 15:20:45 +0900

    Refactoring

--- static/app/scripts/app.js
+++ static/app/scripts/app.js
@@ -19,45 +19,46 @@ angular
     "ui.ace",
     "hc.marked"
   ])
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/");
+  .config(["$stateProvider", "$urlRouterProvider",
+    function ($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise("/");
 
-    $stateProvider
-      .state("home", {
-        url: "/",
-        templateUrl: "views/main.html",
-        controller: "MainCtrl",
-        controllerAs: "mainCtrl"
-      })
-      .state("login", {
-        url: "/login",
-        templateUrl: "views/login.html",
-        controller: "LoginCtrl",
-        controllerAs: "loginCtrl"
-      })
-      .state("viewer", {
-        url: "/viewer",
-        templateUrl: "views/viewer.html",
-        controller: "ViewerCtrl",
-        controllerAs: "viewerCtrl"
-      })
-      .state("viewer.detail", {
-        url: "/:noteId",
-        views: {
-          detail: {
-            templateUrl: "views/viewer_detail.html",
-            controller: "ViewerDetailCtrl",
-            controllerAs: "viewerDetailCtrl"
+      $stateProvider
+        .state("home", {
+          url: "/",
+          templateUrl: "views/main.html",
+          controller: "MainCtrl",
+          controllerAs: "mainCtrl"
+        })
+        .state("login", {
+          url: "/login",
+          templateUrl: "views/login.html",
+          controller: "LoginCtrl",
+          controllerAs: "loginCtrl"
+        })
+        .state("viewer", {
+          url: "/viewer",
+          templateUrl: "views/viewer.html",
+          controller: "ViewerCtrl",
+          controllerAs: "viewerCtrl"
+        })
+        .state("viewer.detail", {
+          url: "/:noteId",
+          views: {
+            detail: {
+              templateUrl: "views/viewer_detail.html",
+              controller: "ViewerDetailCtrl",
+              controllerAs: "viewerDetailCtrl"
+            }
           }
-        }
-      })
-      .state("editor", {
-        url: "/editor/:noteId",
-        templateUrl: "views/editor.html",
-        controller: "EditorCtrl",
-        controllerAs: "editorCtrl"
-      });
-  })
+        })
+        .state("editor", {
+          url: "/editor/:noteId",
+          templateUrl: "views/editor.html",
+          controller: "EditorCtrl",
+          controllerAs: "editorCtrl"
+        });
+  }])
   .constant("API_ROOT", "http://localhost:8000/api")
   .config(["RestangularProvider", "API_ROOT",
     function (RestangularProvider, API_ROOT) {
--- static/app/scripts/controllers/editor.js
+++ static/app/scripts/controllers/editor.js
@@ -18,6 +18,6 @@ angular.module("scribbleApp")
       };
 
       $scope.aceChange = function() {
-      }
+      };
     }
   ]);
